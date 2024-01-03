dab330dc85271815509cdfddb418d23fad41c791
dongyoon7212 <dongyoon7212@naver.com>
2024-01-03 10:08:16 +0900

    복합대입연산, 증간연산

---
+++ src/main/java/com/study/ch04/Operation09.java
@@ -0,0 +1,18 @@
+package com.study.ch04;
+
+public class Operation09 {
+    public static void main(String[] args) {
+        /*
+
+        << 증강연산자 >>
+
+         */
+
+        int i = 0;
+        System.out.println(i); //++i 선증강연산 i++ 후증강연산
+        System.out.println(++i);
+        System.out.println(i--); //대부분 후증간연산
+        System.out.println(i);
+
+    }
+}
