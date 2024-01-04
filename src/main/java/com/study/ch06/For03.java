f17f61803a624159885dfb5d413de98d50a96d0a
dongyoon7212 <dongyoon7212@naver.com>
2024-01-04 11:27:47 +0900

    별찍기

--- src/main/java/com/study/ch06/For03.java
+++ src/main/java/com/study/ch06/For03.java
@@ -20,16 +20,65 @@ public class For03 {
         * 역으로 별찍기
         * */
 
+        System.out.println();
+        System.out.println();
+
+        for(int i = 0; i < starCount; i++) {
+            for(int j = 0; j < starCount - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
         /*
         * 들여쓰기 별찍기
         * */
+
+        System.out.println();
+        System.out.println();
+
+        for (int i = 0; i < starCount; i++) {
+            for (int j = 0; j < starCount - 1 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i + 1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
         /*
         * 들여쓰기 역으로 별찍기
         * */
 
+        System.out.println();
+        System.out.println();
+
+        for (int i = 0; i < starCount; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < starCount - i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
         /*
         * 트리만들기
         * */
+
+        System.out.println();
+        System.out.println();
+
+        for (int i = 0; i < starCount; i++) {
+            for (int j = 0; j < starCount - 1 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < (i + 1) * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
