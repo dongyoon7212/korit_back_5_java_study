96d0094c3b7e3c7f1c4d7c9000bfc9f10d76c73e
dongyoon7212 <dongyoon7212@naver.com>
2024-01-09 12:02:22 +0900

    인터페이스 및 static

---
+++ src/main/java/com/study/ch11/Phone.java
@@ -0,0 +1,25 @@
+package com.study.ch11;
+
+import com.study.ch10.Power;
+
+public class Phone {
+    static int autoCount = 0;
+    int serialNumber;
+    Company company;
+
+    //자동으로 증가할 수 있는 로직 => 생성될때 카운트가 되어서 시리얼 넘버를 증강시킴
+    public Phone(Company company) {
+        autoCount++;
+        this.serialNumber = 20240000;
+        this.serialNumber += autoCount;
+        this.company = company;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "serialNumber=" + serialNumber +
+                ", company=" + company.toString() +
+                '}';
+    }
+}
---
+++ src/main/java/com/study/ch11/SamsungCompany.java
@@ -0,0 +1,10 @@
+package com.study.ch11;
+
+public class SamsungCompany {
+    //static => 스택메모리에 저장
+    static String name = "삼성";
+
+    static void showCompanyInfo() {
+        System.out.println("회사명: " + name);
+    }
+}
